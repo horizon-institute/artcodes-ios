b551cd6db38d5cce2e2410c34d484523573f5b7c
Kevin Glover <ktg@cs.nott.ac.uk>
2015-01-15 11:10:55 +0000

    Update copyright

--- View Controllers/CameraViewController.h
+++ View Controllers/CameraViewController.h
@@ -1,11 +1,21 @@
-//
-//  ACFirstViewController.h
-//  aestheticodes
-//
-//  Created by horizon on 17/07/2013.
-//  Copyright (c) 2013 horizon. All rights reserved.
-//
-
+/*
+ * Aestheticodes recognises a different marker scheme that allows the
+ * creation of aesthetically pleasing, even beautiful, codes.
+ * Copyright (C) 2015  Aestheticodes
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #import <UIKit/UIKit.h>
 #import <opencv2/highgui/cap_ios.h>
 #import "ECSlidingViewController.h"
