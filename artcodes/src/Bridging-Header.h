1577b25c9a6f8236d9e04cdb9e55734ae206de02
Kevin Glover <ktg@cs.nott.ac.uk>
2015-09-22 14:58:48 +0100

    Fix copyright notices

--- artcodes/src/Bridging-Header.h
+++ artcodes/src/Bridging-Header.h
@@ -1,17 +1,26 @@
-//
-//  Bridging-Header.h
-//  artcodes
-//
-//  Created by Kevin Glover on 03/08/2015.
-//  Copyright (c) 2015 Horizon. All rights reserved.
-//
+/*
+ * Artcodes recognises a different marker scheme that allows the
+ * creation of aesthetically pleasing, even beautiful, codes.
+ * Copyright (C) 2013-2015  The University of Nottingham
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef artcodes_Bridging_Header_h
 #define artcodes_Bridging_Header_h
 
 #import <Google/Analytics.h>
 #import <Google/SignIn.h>
-
-#import <CarbonKit/CarbonKit.h>
 
 #endif
